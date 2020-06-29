f84851e486803e5add0aa52ec38322ad04140b38
Emannuel Matos <emannuelloxt@hotmail.com>
2020-06-29 00:16:57 -0400

    :sparkles: feat(signup page): verify username, email, password and confirm-password

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -10,10 +10,36 @@ export class SignupComponent implements OnInit {
   email: string = '';
   password: string = '';
   confirmPassword: string = '';
+  valid = {
+    username: true,
+    password: true,
+    email: true,
+  };
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  validate(type: string): void {
+    const usernamePattern = /^[\w-.]*$/;
+    const emailPattern = /\S+@\S+\.\S+/;
+
+    if (type === 'username') {
+      if (this.username.length < 5) {
+        this.valid.username = false;
+      } else {
+        this.valid.username = usernamePattern.test(this.username);
+      }
+    } else if (type === 'email') {
+      this.valid.email = emailPattern.test(this.email);
+    } else if (type === ('confirmPassword' || 'password')) {
+      if (this.password.length < 5) {
+        this.valid.password = false;
+      } else {
+        this.valid.password = this.password === this.confirmPassword;
+      }
+    }
+  }
 
   change(e: any, type: string) {
     if (type === 'username') {
@@ -25,9 +51,15 @@ export class SignupComponent implements OnInit {
     } else if (type === 'confirmPassword') {
       this.confirmPassword = e.target.value;
     }
+
+    this.validate(type);
   }
 
   onSubmit(e: Event) {
     e.preventDefault();
+
+    if (this.valid.username && this.valid.email && this.valid.password) {
+      alert('Your account has been created!');
+    }
   }
 }
