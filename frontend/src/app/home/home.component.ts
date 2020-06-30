52b2de854d93d801c63f48eca497865d04ed31a6
Emannuel Matos <emannuelloxt@hotmail.com>
2020-06-30 10:49:26 -0400

    :sparkles: feat(home): redirect to login or signup

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  user: boolean = false;
 
-  ngOnInit(): void {}
+  constructor() {}
+  ngOnInit(): void {
+    this.user = localStorage.getItem('user') != null;
+  }
 }
